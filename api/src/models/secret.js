212d863534b8e8a06d648133390a8832368db9a2
Mohamed Jerbi <mohamed.jerbi2012@gmail.com>
2022-03-25 17:32:28 +0100

    Chor: removed unused code

--- api/src/models/secret.js
+++ api/src/models/secret.js
@@ -15,32 +15,4 @@ const secretSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-secretSchema.pre('save', function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  console.log('not reached');
-  bcrypt.hash(this.password, 8, (err, hash) => {
-    if (err) {
-      return next(err);
-    }
-
-    this.password = hash;
-    next();
-  });
-});
-
-userSchema.methods.checkPassword = function (password) {
-  const passwordHash = this.password;
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, passwordHash, (err, same) => {
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(same);
-    });
-  });
-};
-
 export default mongoose.model('secret', secretSchema);
