6da699f82d6501ca29532f829c891f337303a87b
Mohamed Jerbi <mohamed.jerbi2012@gmail.com>
2022-03-25 21:38:33 +0100

    Feat: added routes validation

--- api/src/routes/secret.route.js
+++ api/src/routes/secret.route.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { getSecret, createSecret } from '../controllers/secret.controller';
+import {
+  getSecretValidation,
+  createSecretValidation,
+} from '../validators/secret.validation';
 
 const router = Router();
 
-router.get('/secret/:hash', getSecret);
-router.post('/secret', createSecret);
+router.get('/secret/:hash', getSecretValidation, getSecret);
+router.post('/secret', createSecretValidation, createSecret);
 
 export default router;
---
+++ api/src/validators/secret.validation.js
@@ -0,0 +1,28 @@
+import * as yup from 'yup';
+
+const getSecretSchema = yup.object().shape({
+  hash: yup.string().required().length(24),
+});
+
+const createSecretSchema = yup.object().shape({
+  secretText: yup.string().required(),
+  expiresAfter: yup.number().required(),
+});
+
+export const getSecretValidation = async (req, res, next) => {
+  if (await getSecretSchema.isValid(req.params)) {
+    return next();
+  }
+  return res.status(500).send({ error: 'Unvalid hash' });
+};
+
+export const createSecretValidation = async (req, res, next) => {
+  if (await createSecretSchema.isValid(req.body)) {
+    return next();
+  }
+  return res.status(500).send({
+    error: 'Must provide a valid secretText and expiresAfter parameters',
+  });
+};
+
+export default { getSecretValidation, createSecretValidation };
