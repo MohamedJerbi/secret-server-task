b4672199fd4b2af54cd6cdf1673aa125819928b8
Mohamed Jerbi <mohamed.jerbi2012@gmail.com>
2022-03-26 17:07:20 +0100

    Cho: updated site title and default config

--- app/src/config/defaultConfigs.ts
+++ app/src/config/defaultConfigs.ts
@@ -1,6 +1,6 @@
 // in case no envirement variable is present
 
-export const API_URL: string = "http://localhost:3333/secret";
+export const API_URL: string = "https://tooooosecret.herokuapp.com/secret";
 
 export const configs = { API_URL };
 
