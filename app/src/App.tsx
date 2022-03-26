9f7033217da1673c6b6427719ccdcb5c3308953c
Mohamed Jerbi <mohamed.jerbi2012@gmail.com>
2022-03-26 11:46:55 +0100

    Chor: UI enhancement and error management

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,16 @@
 import React, { useCallback, useState } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Grid,
+  Input,
+  TextField,
+} from "@mui/material";
 import createSecret from "./queries/addSecret";
 import Secret from "./types/Secret";
+import Alert from "@mui/material/Alert";
 import { API_URL } from "./config/defaultConfigs";
 import "./App.css";
 
@@ -9,6 +19,9 @@ function App() {
   const [expiresAfter, setExpiresAfter] = useState<number>(0);
   const [secretGeneratedUrl, setGeneratedSecretUrl] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState<boolean | null>(null);
+  const [submit, setSubmit] = useState<boolean>(false);
 
   const handleSetSecret = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,25 +37,105 @@ function App() {
     []
   );
 
+  const handleSetError = useCallback((message: string) => {
+    setError(message);
+    setTimeout(() => setError(""), 5000);
+  }, []);
+
   const handleGenerateSecretUrl = useCallback(() => {
-    createSecret({ secretText, expiresAfter }).then(
-      ({ data: { hash } }: { data: Secret }) =>
-        setGeneratedSecretUrl(`${process.env.API_URL || API_URL}/${hash}`),
-      (error) => setError(error.toString())
-    );
-  }, [secretText, expiresAfter]);
-  console.log(process.env.API_URL, API_URL);
+    if (!secretText) {
+      document.getElementById("secretInput")?.focus();
+      handleSetError("Please enter a secret");
+      setTimeout(() => setError(""), 5000);
+      return setSubmit(true);
+    }
+    setLoading(true);
+    createSecret({ secretText, expiresAfter })
+      .then(
+        ({ data: { hash } }: { data: Secret }) => {
+          setGeneratedSecretUrl(`${process.env.API_URL || API_URL}/${hash}`);
+          setSuccess(true);
+          setTimeout(() => setSuccess(null), 5000);
+        },
+        (error) => {
+          handleSetError(error.toString());
+        }
+      )
+      .finally(() => setLoading(false));
+  }, [secretText, expiresAfter, handleSetError]);
+
   return (
-    <div className="App">
-      <input value={secretText} onChange={handleSetSecret} />
-      <input
-        type={"number"}
-        value={expiresAfter}
-        onChange={handleSetExpiresAfter}
-      />
-      <button onClick={handleGenerateSecretUrl}>Generate secret URL</button>
-      {secretGeneratedUrl && <p>{secretGeneratedUrl}</p>}
-      {error && <p>{error}</p>}
+    <div>
+      {error && <Alert severity="error">{error.toString()}</Alert>}
+      {success && (
+        <Alert severity="success">
+          Your secret url was generated succefully!
+        </Alert>
+      )}
+      <Container maxWidth="sm">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2} style={{ marginTop: "20vh" }}>
+            <Grid className="flex-center" item xs={6}>
+              <TextField
+                id="secretInput"
+                label="Secret Text"
+                placeholder="my secret"
+                value={secretText}
+                onChange={handleSetSecret}
+                autoFocus
+                fullWidth
+                error={submit && secretText.length === 0}
+              />
+            </Grid>
+            <Grid className="flex-center" item xs={3}>
+              <TextField
+                label="Expires After"
+                type={"number"}
+                value={expiresAfter}
+                onChange={handleSetExpiresAfter}
+              />
+            </Grid>
+            <Grid className="flex-center" item xs={3}>
+              {loading ? (
+                <CircularProgress className="flex-center" />
+              ) : (
+                <Button variant="contained" onClick={handleGenerateSecretUrl}>
+                  Generate secret URL
+                </Button>
+              )}
+            </Grid>
+            <Grid className="flex-center" item xs={12}>
+              <p>*Expiration Time (in seconds), 0 means never expire</p>
+            </Grid>
+            {secretGeneratedUrl && (
+              <>
+                <Grid className="flex-center" item xs={8}>
+                  <Input value={secretGeneratedUrl} fullWidth />
+                </Grid>
+                <Grid className="flex-center" item xs={2}>
+                  <Button
+                    variant="outlined"
+                    onClick={() =>
+                      navigator.clipboard.writeText(secretGeneratedUrl)
+                    }
+                  >
+                    Copy
+                  </Button>
+                </Grid>
+                <Grid className="flex-center" item xs={2}>
+                  <Button
+                    variant="outlined"
+                    href={secretGeneratedUrl}
+                    target="_blank"
+                  >
+                    Open
+                  </Button>
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
 }
